4d5cfdb7af4a547df9ffd14887c7c3bcaa7d88e3
ModouLaminJagne <jagnemlg@gmai.com>
2023-08-30 13:33:35 +0000

    Write a simple web server.

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,5 +1,7 @@
-const fs = require('fs')
+const fs = require('fs');
+const http = require('http');
 
+///////////////////////////// FILES //////////////////////////////
 //***************************** READING AND WRITING FILES *****************************//
 
 //***************************** Blocking, synchronous code ******************************//
@@ -14,19 +16,29 @@ const fs = require('fs')
 
 //***************************** Non-blocking, asynchronous way *************************//
 //reading
-fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data) => {
+// fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data) => {
 
-    if (err) return console.log('ERROR! 💥')
+//     if (err) return console.log('ERROR! 💥');
 
-    fs.readFile(`./starter/txt/${data}.txt`, 'utf-8', (err, data1) => {
-        console.log(data1)
-        fs.readFile(`./starter/txt/append.txt`, 'utf-8', (err, data2) => {
-            console.log(data2)
+//     fs.readFile(`./starter/txt/${data}.txt`, 'utf-8', (err, data1) => {
+//         console.log(data1);
+//         fs.readFile(`./starter/txt/append.txt`, 'utf-8', (err, data2) => {
+//             console.log(data2);
 
-            fs.writeFile('./starter/txt/final.txt', `${data1}\n${data2}`, 'utf-8', err => {
-                console.log('Your fie has been written 😊')
-            })
-        })
-    })
+//             fs.writeFile('./starter/txt/final.txt', `${data1}\n${data2}`, 'utf-8', err => {
+//                 console.log('Your fie has been written 😊');
+//             });
+//         });
+//     });
+// });
+// console.log("File is being read!");
+
+///////////////////////////////// SERVERS /////////////////////////////
+const server = http.createServer((req, res) => {
+    console.log(req);
+    res.end('Hello from the server!');
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('listening to request on port 8000');
 })
-console.log("File is being read!")
